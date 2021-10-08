448098e166c5ccc39af612e45c2db14428a777bd
Abraham Haros <abrahamharos@Abrahams-MacBook-Pro.local>
2021-10-07 23:37:22 -0700

    Add function directory functionality

--- MyRLike_parse.py
+++ MyRLike_parse.py
@@ -2,12 +2,32 @@ import sys
 import MyRLike_lex
 tokens = MyRLike_lex.tokens
 from ply import yacc
+import pprint
+
+# TODO: Move this declaration/functions to a separate file.
+functionDirectory = {}
+currentType = ''
+currentVariableName = ''
+currentFunction = ''
+programName = ''
 
 def p_program(p):
-    '''program      : PROGRAM ID SEMI body
-                    | PROGRAM ID SEMI vars_dec body
-                    | PROGRAM ID SEMI vars_dec func_dec body'''
-    print(list(p))
+    '''program      : PROGRAM ID save_program_data SEMI body
+                    | PROGRAM ID save_program_data SEMI vars_dec body
+                    | PROGRAM ID save_program_data SEMI vars_dec func_dec body'''
+
+def p_save_program_data(p):
+    '''save_program_data : '''
+    
+    global programName, currentType, currentFunction
+    
+    programName = p[-1]
+    currentFunction = p[-1]
+    currentType = 'program'
+
+    functionDirectory[currentFunction] = {
+        'type': currentType
+    }
 
 def p_body(p):
     '''body         : MAIN LPAREN RPAREN block'''
@@ -129,7 +149,9 @@ def p_type(p):
     '''type         : TYPEINT
                     | TYPEFLOAT
                     | TYPECHAR'''
-    pass
+    
+    global currentType
+    currentType = p[1]
 
 def p_vars_md_id(p):
     '''vars_md_id   : LBRACKET INT RBRACKET vars_md_id
@@ -138,9 +160,23 @@ def p_vars_md_id(p):
 
 def p_vars_id_dec(p):
     '''vars_id_dec  : vars_id_dec COMMA vars_id_dec
-                    | ID
-                    | ID vars_md_id'''
-    pass
+                    | ID save_variable_name
+                    | ID save_variable_name vars_md_id'''
+
+def p_save_variable_name(p):
+    '''save_variable_name : '''
+    
+    global functionDirectory, currentFunction, currentType, currentVariableName
+    localVariableName = p[-1]
+    
+    if localVariableName in functionDirectory[currentFunction]['vars']:
+        # Throw Multiple declaration error
+        print("Variable " + localVariableName + " declared multiple times in the same scope")
+        exit()
+    else:
+        functionDirectory[currentFunction]['vars'][localVariableName] = {
+            'type': currentType
+        }
 
 def p_vars_body(p):
     '''vars_body    : vars_body vars_body
@@ -148,8 +184,17 @@ def p_vars_body(p):
     pass
 
 def p_vars_dec(p):
-    '''vars_dec     : VARS vars_body'''
-    pass
+    '''vars_dec     : VARS save_VARTable vars_body'''
+
+def p_save_VARTable(p):
+    '''save_VARTable : '''
+
+    global currentFunction, functionDirectory
+    
+    # Check if current_function already has a var table.
+    # If not, initialize one.
+    if not("vars" in functionDirectory[currentFunction].keys()):
+        functionDirectory[currentFunction]['vars'] = {}
 
 def p_func_params(p):
     '''func_params  :   func_params COMMA func_params
@@ -159,15 +204,40 @@ def p_func_params(p):
 def p_func_type(p):
     '''func_type    :   type
                     |   VOID'''
-    pass
+    
+    # When p[1] is set it means that is a void function since
+    # other types are being handle in p_type
+    if (p[1]):
+        global currentType
+        currentType = p[1]
 
 def p_func_dec(p):
     '''func_dec     :   func_dec func_dec
-                    |   FUNC func_type ID LPAREN RPAREN vars_dec block
-                    |   FUNC func_type ID LPAREN func_params RPAREN vars_dec block
-                    |   FUNC func_type ID LPAREN RPAREN block
-                    |   FUNC func_type ID LPAREN func_params RPAREN block'''
-    pass
+                    |   FUNC func_type ID save_function_data LPAREN RPAREN vars_dec block
+                    |   FUNC func_type ID save_function_data LPAREN func_params RPAREN vars_dec block
+                    |   FUNC func_type ID save_function_data LPAREN RPAREN block
+                    |   FUNC func_type ID save_function_data LPAREN func_params RPAREN block'''
+
+def p_save_function_data(p):
+    '''save_function_data : '''
+
+    global functionDirectory
+
+    localFunctionName = p[-1]
+    # Check if functionDirectory already has a function with the same name
+    # If not, initialize one.
+    if localFunctionName in functionDirectory.keys():
+        # Throw Multiple declaration error
+        print("Function " + localFunctionName + " declared multiple times")
+        exit()
+
+    global currentType, currentFunction
+    
+    currentFunction = p[-1]
+
+    functionDirectory[currentFunction] = {
+        'type': currentType
+    }
 
 def p_return(p):
     '''return       :   RETURN LPAREN exp RPAREN'''
